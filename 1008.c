2106d52334b28fef8af77d2c7deb60aa807f77ac
ksks723 <ksks723@gmail.com>
2021-11-13 01:13:50 +0900

    출력

---
+++ 1008.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double a, b;
+    scanf("%d %d", &a, &b);
+    printf("%.9f", a / b);
+    return 0;
+}
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("진짜된다dddddddddddddddddddd고????");
-    return 0;
-}
