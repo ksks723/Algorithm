e8e737d68ec8e191398a6f0a3cdc043993879109
ksks723 <ksks723@gmail.com>
2021-11-14 23:36:09 +0900

    comtti

---
+++ 10818.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    int min = 10000001;
+    int max = -1000001;
+    int input = 0;
+
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("&d", &input);
+
+        if (input < min)
+        {
+            min = input;
+        }
+        if (input > max)
+        {
+            max = input;
+        }
+    }
+    printf("&d &d", min, max);
+    return 0;
+}
