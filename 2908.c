eea7f2772c9ceb4382ca7a73c2e1c7e15d780374
ksks723 <ksks723@gmail.com>
2021-11-22 22:41:24 +0900

    ck

--- 2908.c
+++ 2908.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
-    char one[3];
-    char two[3];
+    char one[4];
+    char two[4];
     char tmp;
+
+    scanf("%s", &one);
+    scanf("%s", &two);
+    tmp = one[0];
+    one[0] = one[2];
+    one[2] = tmp;
+
+    tmp = two[0];
+    two[0] = two[2];
+    two[2] = tmp;
+
+    if (strcmp(one, two) > 0)
+    {
+        printf("%s", one);
+    }
+    else
+    {
+        printf("%s", two);
+    }
 }
