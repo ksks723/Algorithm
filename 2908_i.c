ab43d6dc4773d0f57358c0a8979ae1da3f731045
ksks723 <ksks723@gmail.com>
2021-11-22 23:02:57 +0900

    수정

--- 2908_i.c
+++ 2908_i.c
@@ -5,7 +5,7 @@ int main(void)
     int one, two;
     int il, e;
 
-    scanf("%d %d", one, two);
+    scanf("%d %d", &one, &two);
 
     il = (one % 10 * 100) + (one / 10 % 10 * 10) + (one / 100);
     e = (two % 10 * 100) + (two / 10 % 10 * 10) + (two / 100);
