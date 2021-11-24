5c2d65ed479fddd90b8e2ae4a92eb7dd120bec7a
ksks723 <ksks723@gmail.com>
2021-11-24 16:30:18 +0900

    ho

--- 1978.c
+++ 1978.c
@@ -2,9 +2,38 @@
 
 int main(void)
 {
-    int num, max, sum = 0, last = 0;
-    int card[100] = {0};
-    scanf("%d %d", &num, &max);
+    int N, a, cnt = 0;
+    int ckPrime[101] = {0};
+    int isPrime[1001] = {0};
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &ckPrime[i]);
+    }
 
-    printf("%d", last);
+    for (int i = 2; i < 1001; i++)
+    {
+        isPrime[i] = i;
+    }
+
+    for (int i = 2; i * 2 < 1001; i++)
+    {
+        if (isPrime[i] == 0)
+        {
+            continue;
+        }
+        for (int j = i, k = i; j * k < 1001; k++)
+        {
+            isPrime[j * k] = 0;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (isPrime[ckPrime[i]] != 0)
+        {
+            cnt++;
+        }
+    }
+    printf("%d", cnt);
 }
