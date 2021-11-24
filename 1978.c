581b189aa1dc24034f0a0df10eb24bbfedfd62a3
ksks723 <ksks723@gmail.com>
2021-11-24 15:18:18 +0900

    dd

---
+++ 1978.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num, max, sum = 0, last = 0;
+    int card[100] = {0};
+    scanf("%d %d", &num, &max);
+
+    printf("%d", last);
+}
--- 2798.c
+++ 2798.c
@@ -2,12 +2,26 @@
 
 int main(void)
 {
-    int num, max;
-    int length;
-    int card[100] =
-        scanf("%d %d", &num, &max);
-    for (int i = 0; i < length; i++)
+    int num, max, sum = 0, last = 0;
+    int card[100] = {0};
+    scanf("%d %d", &num, &max);
+    for (int i = 0; i < num; i++)
     {
-        scanf("%d", )
+        scanf("%d", &card[i]);
     }
+    for (int i = 0; i < num - 2; i++)
+    {
+        for (int j = i + 1; j < num - 1; j++)
+        {
+            for (int k = j + 1; k < num; k++)
+            {
+                sum = card[i] + card[j] + card[k];
+                if (card[i] + card[j] + card[k] <= max && last < sum)
+                {
+                    last = sum;
+                }
+            }
+        }
+    }
+    printf("%d", last);
 }
