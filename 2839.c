266e590b8e6a724255f7d673d5aa0e7cf5186867
ksks723 <ksks723@gmail.com>
2021-11-26 19:00:19 +0900

    2839

--- 2839.c
+++ 2839.c
@@ -6,24 +6,41 @@ int main(void)
     int n, cnt = 0;
     scanf("%d", &n);
 
-    while (n > 0)
+    // while (n > 0)
+    // {
+    //     if (n % 5 == 0)
+    //     {
+    //         n -= 5;
+    //     }
+    //     else
+    //     {
+    //         n -= 3;
+    //     }
+    //     cnt++;
+    // }
+    // if (n < 0 || cnt == 0)
+    // {
+    //     printf("-1");
+    // }
+    // else
+    // {
+    //     printf("%d", cnt);
+    // }
+
+    while (1)
     {
         if (n % 5 == 0)
         {
-            n -= 5;
+            cnt += n / 5;
+            printf("%d", cnt);
+            break;
         }
-        else
-        {
-            n -= 3;
-        }
+        n -= 3;
         cnt++;
-    }
-    if (n < 0 || cnt == 0)
-    {
-        printf("-1");
-    }
-    else
-    {
-        printf("%d", cnt);
+        if (n < 0)
+        {
+            printf("-1");
+            break;
+        }
     }
 }
