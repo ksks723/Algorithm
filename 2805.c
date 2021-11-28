16d20a2dcd99f7fad3898365fdc0806e247ab13c
ksks723 <ksks723@gmail.com>
2021-11-28 21:58:57 +0900

    2805

--- 2805.c
+++ 2805.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
 #include <string.h>
-
+int tree[1000001];
 int main(void)
 {
-    int gru, m;
-    scanf("%d %d", &gru, &m);
+    int n, m, max = 0, left = 0, right, res = 0;
+    scanf("%d %d", &n, &m);
 
-    for (int i = 0; i < gru, i++)
+    for (int i = 0; i < n; i++)
     {
-        int a, b, c, d;
-        scanf("%d %d %d %d", &a, &b, &c, &d);
+        scanf("%d", &tree[i]);
+        if (max < tree[i])
+        {
+            max = tree[i];
+        }
     }
+
+    left = 0;
+    right = max;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (tree[i] > mid)
+            {
+                res += tree[i] - mid;
+            }
+        }
+        if (res == m)
+        {
+            res = mid;
+            left += 1;
+        }
+        else
+        {
+            right -= 1;
+            res = 0;
+        }
+    }
+    printf("%d", res);
 }
