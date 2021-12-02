8e67dd3f4338efd902c8dd35dff70e8171286a91
ksks723 <ksks723@gmail.com>
2021-12-02 18:47:24 +0900

    1259

---
+++ 1259.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+int main(void)
+{
+
+    while (1)
+    {
+        bool ck = true;
+        char num[10];
+        scanf("%s", &num);
+        if (num[0] == '0')
+        {
+            break;
+        }
+        int len = strlen(num);
+        for (int i = 0; i < len / 2; i++)
+        {
+            if (num[i] != num[len - 1 - i])
+            {
+                ck = false;
+            }
+        }
+        if (ck)
+        {
+            printf("yes\n");
+        }
+        else
+        {
+            printf("no\n");
+        }
+    }
+}
--- 1259.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <stack>
-using namespace std;
-
-int main(void)
-{
-    stack<char> st;
-}
