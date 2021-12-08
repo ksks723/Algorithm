5856ead8e1ee656955b18a10424a68a0bcddf6e0
ksks723 <ksks723@gmail.com>
2021-12-08 21:09:45 +0900

    1213

--- 1213.cpp
+++ 1213.cpp
@@ -2,30 +2,36 @@
 #include <string>
 #include <algorithm>
 using namespace std;
-int c[128];
-string t, l, r;
+int alpha[97];
+string hol, zzac;
 int main()
 {
     string s;
     cin >> s;
     for (char i : s)
     {
-        c[i]++;
+        alpha[i]++;
     }
-    for (char i = 'A'; i < 'Z'; ++i)
+    for (char i = 'A'; i <= 'Z'; i++)
     {
-        if (c[i] % 2)
-            t += i;
-        for (int j = 0; j < c[i] / 2; ++j)
-            l += i;
+        if (alpha[i] % 2)
+        {
+            hol += i;
+        }
+        for (int j = 0; j < alpha[i] / 2; j++)
+        {
+            zzac += i;
+        }
     }
-    if (t.size() > 1)
-        cout << "I'm Sorry Hansoo";
+    if (hol.size() > 1)
+    {
+        printf("I'm Sorry Hansoo");
+    }
     else
     {
-        cout << l;
-        cout << t;
-        reverse(l.begin(), l.end());
-        cout << l;
+        cout << zzac;
+        cout << hol;
+        reverse(zzac.begin(), zzac.end());
+        cout << zzac;
     }
 }
