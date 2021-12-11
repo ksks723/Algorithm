6fd6649dcdf3520261e2e3c7df118ce574df501e
ksks723 <ksks723@gmail.com>
2021-12-11 12:30:59 +0900

    10773

--- 10773.cpp
+++ 10773.cpp
@@ -4,10 +4,28 @@ using namespace std;
 int main(void)
 {
     stack<int> s;
-    int k;
+    int k, sum = 0;
 
     cin >> k;
     for (int i = 0; i < k; i++)
     {
+        int n;
+        cin >> n;
+        if (n == 0)
+        {
+            s.pop();
+        }
+        else
+        {
+            s.push(n);
+        }
     }
+
+    while (!s.empty())
+    {
+
+        sum += s.top();
+        s.pop();
+    }
+    cout << sum;
 }
