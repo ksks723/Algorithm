c01b38e0474b15819148bc77ea58ff47b332eecc
ksks723 <ksks723@gmail.com>
2021-12-13 16:12:40 +0900

    1920

---
+++ 1920.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int main(void)
+{
+    fast_io();
+    int k;
+
+    cin >> k;
+    for (int i = 0; i < k; i++)
+    {
+        int n;
+        cin >> n;
+    }
+}
