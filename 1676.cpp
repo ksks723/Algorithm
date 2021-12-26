0f83cefc85e9cb19e82d28caa91a4e653508ad90
ksks723 <ksks723@gmail.com>
2021-12-26 14:11:58 +0900

    1676

--- 1676.cpp
+++ 1676.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-#include <utility>
-using namespace std;
 
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
 int main(void)
 {
+    fast_io();
+    int a;
+    int res = 0;
+    cin >> a;
+    res = a / 5 + a / 25 + a / 125;
+    cout << res;
 }
