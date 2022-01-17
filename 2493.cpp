70ac2a8620aac19223e5ca9126e7a246d3d433e4
ksks723 <ksks723@gmail.com>
2022-01-17 22:06:38 +0900

    2493

---
+++ 2493.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int main(void)
+{
+    fast_io();
+ 
+}
