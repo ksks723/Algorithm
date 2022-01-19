88d5c2d7a0196ee75d233fb688a86c62b758b589
ksks723 <ksks723@gmail.com>
2022-01-19 22:04:19 +0900

    1205

--- 1205.cpp
+++ 1205.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
-#include <deque>
+#include <algorithm>
+#include <vector>
 using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
 int main()
 {
+    vector<int> v;
+    fast_io();
+    int n, score, p, res = 0; // 랭킹목록, 태수점수, 랭킹개수
+    cin >> n >> score >> p;
+    if (n == 0)
+    {
+        cout << "1";
+        return 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        long long a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.rbegin(), v.rend());
+    if (v.back() >= score && v.size() == p)
+    {
+        cout << "-1";
+    }
+
+    else
+    {
+        v.push_back(score);
+        sort(v.rbegin(), v.rend());
+        for (int i = 0; i < v.size(); i++)
+        {
+            if (v[i] == score)
+            {
+                res = i;
+                break;
+            }
+        }
+        cout << res + 1;
+    }
 }
