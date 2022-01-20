d932d199bc90bef69bc978a909ef6f6944cd3236
ksks723 <ksks723@gmail.com>
2022-01-20 23:21:47 +0900

    21156

---
+++ 21156.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int main()
+{
+    fast_io();
+    list<int> l;
+}
