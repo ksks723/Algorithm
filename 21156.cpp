10730ea6c6860d226c9eee038bd819d65ea52c34
ksks723 <ksks723@gmail.com>
2022-01-21 15:33:15 +0900

    21156

--- 21156.cpp
+++ 21156.cpp
@@ -12,4 +12,52 @@ int main()
 {
     fast_io();
     list<int> l;
+    std::list<int>::iterator it;
+    //  12345
+    //  4 1  -> 23415
+    //  3 1  -> 23415
+    //  5 3  -> 24153
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        l.push_back(i);
+    while (m--)
+    {
+        it = l.begin();
+        string a, b;
+        cin >> a >> b;
+        int winner = stoi(a.substr(1));
+        int loser = stoi(b.substr(1));
+        int i = 0, ck = 0;
+        for (auto it = l.begin(); it != l.end(); it++)
+        {
+            if (*it == loser && i == 1)
+            {
+                ck = 1;
+                break;
+            }
+            else if (*it == winner)
+                i = 1;
+        }
+        if (ck == 0)
+        {
+            while (it != l.end())
+            {
+                if (*it == loser)
+                {
+                    it = l.erase(it++);
+                }
+                else if (*it == winner)
+                {
+                    l.insert(++it, loser);
+                }
+                else
+                {
+                    it++;
+                }
+            }
+        }
+    }
+    for (auto it = l.begin(); it != l.end(); it++)
+        cout << "T" << *it << " ";
 }
