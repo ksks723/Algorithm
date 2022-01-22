f46e4a413be3a07b6ea3a2c952a5a835d3474f66
ksks723 <ksks723@gmail.com>
2022-01-23 00:49:42 +0900

    d

--- 1487.cpp
+++ 1487.cpp
@@ -11,7 +11,7 @@ void fast_io(void)
 int main(void)
 {
     fast_io();
-    int n, max = 0, idx = 0;
+    long long n, max = 0, idx = 0;
     vector<pair<int, int>> v;
     cin >> n;
     int arr[51];
@@ -21,23 +21,48 @@ int main(void)
         cin >> a >> b;
         v.push_back(make_pair(a, b));
     }
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i].first - v[i].second)
-            arr[i] = (v[i].first - v[i].second) * (n - i);
-        else
-            arr[i] = 0;
-    }
+    sort(v.begin(), v.end());
+    cout << "정렬후\n";
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > max)
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+
+    int maxp = 0;
+    int tmaxp = 0;
+    for (int i = 0; i < n; i++) //0
+    {
+        int total = 0;
+        for (int j = i; j < n; j++) //0 1 2 3 4
         {
-            max = arr[i];
-            idx = i;
+            int won = v[i].first - v[j].second;
+            if (won > 0)
+                total += won;
+        }
+        if (tmaxp < total)
+        {
+            tmaxp = total;
+            maxp = v[i].first;
         }
     }
-    if (max == 0)
-        cout << "0";
-    else
-        cout << v[idx].first;
+    cout << maxp;
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     if (v[i].first - v[i].second)
+    //         arr[i] = (v[i].first - v[i].second) * (n - i);
+    //     else
+    //         arr[i] = 0;
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] > max)
+    //     {
+    //         max = arr[i];
+    //         idx = i;
+    //     }
+    // }
+    // if (max == 0)
+    //     cout << "0";
+    // else
+    //     cout << v[idx].first;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,19 @@
- 
+for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i].first - v[i].second)
+            arr[i] = (v[i].first - v[i].second) * (n - i);
+        else
+            arr[i] = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            idx = i;
+        }
+    }
+    if (max == 0)
+        cout << "0";
+    else
+    
