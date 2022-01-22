7dd803741f9612c156b9d630267f43e51f67e61e
ksks723 <ksks723@gmail.com>
2022-01-22 22:39:27 +0900

    d

--- 1487.cpp
+++ 1487.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 void fast_io(void)
@@ -7,12 +8,36 @@ void fast_io(void)
     cin.tie(NULL);
     cout.tie(NULL);
 }
-long long int x, y, w, s;
 int main(void)
 {
-    cin >> x >> y >> w >> s;
-    long long mod = (x + y) % 2;
-    if (x < y)
-        swap(x, y);
-    cout << min((x + y) * w, min((x - mod) * s + mod * w, y * s + (x - y) * w)) << endl;
+    fast_io();
+    int n, max = 0, idx = 0;
+    vector<pair<int, int>> v;
+    cin >> n;
+    int arr[51];
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        v.push_back(make_pair(a, b));
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i].first - v[i].second)
+            arr[i] = (v[i].first - v[i].second) * (n - i);
+        else
+            arr[i] = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            idx = i;
+        }
+    }
+    if (max == 0)
+        cout << "0";
+    else
+        cout << v[idx].first;
 }
