9b1216959b7eb2e2f0ccf887a357f72d7e5fd01c
ksks723 <ksks723@gmail.com>
2022-02-01 23:25:54 +0900

    4158

---
+++ 4158.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int main()
+{
+    fast_io();
+}
