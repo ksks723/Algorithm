7dd5311f1b263e3b7f997b03f81fbb022cba616b
ksks723 <ksks723@gmail.com>
2022-02-02 20:43:47 +0900

    4673:

--- 4158.cpp
+++ 4158.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 void fast_io(void)
 {
@@ -9,4 +10,46 @@ void fast_io(void)
 int main()
 {
     fast_io();
+    while (1)
+    {
+        int n, m;
+        int res = 0, num;
+        int posiA = 0, posiB = 0;
+        vector<int> a;
+        vector<int> b;
+
+        cin >> n >> m;
+        if (n == 0 && m == 0)
+        {
+            break;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> num;
+            a.push_back(num);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> num;
+            b.push_back(num);
+        }
+        while (1)
+        {
+            if (posiA == n || posiB == m)
+                break;
+
+            if (a[posiA] < b[posiB])
+            {
+                posiA++;
+            }
+            else if (a[posiA] > b[posiB])
+                posiB++;
+
+            else
+                posiA++;
+            posiB++;
+            res++;
+        }
+        cout << res << '\n';
+    }
 }
