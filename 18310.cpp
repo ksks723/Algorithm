d40fe9bdcab52a4abbe094bd3bc0a2da77c550fc
ksks723 <ksks723@gmail.com>
2022-03-12 17:36:48 +0900

    d

---
+++ 18310.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+vector<int> v;
+int main(void)
+{
+    fast_io();
+    int n, res = 1000001,ans=0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    cout << v[(n-1)/2];
+}
