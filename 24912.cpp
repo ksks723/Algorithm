fd174cf4e28ec83ffe327efbe69478063865e5df
ksks723 <ksks723@gmail.com>
2022-04-03 22:43:44 +0900

    24912

--- 24912.cpp
+++ 24912.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -9,24 +8,32 @@ void fast_io(void)
     cin.tie(NULL);
     cout.tie(NULL);
 }
-int n, arr[1000001],cnt=0;
+int n, arr[1000001];
 int main()
 {
     fast_io();
     cin >> n;
     for (int i = 0; i < n;i++)
-     {   cin >> arr[i];
-        if(arr[i]==0)
-    cnt++;
-        
-     }
-     if(cnt==0)
+      cin >> arr[i];
+
+    for (int i = 0; i < n - 1;i++)
+        if(arr[i] && arr[i] == arr[i+1])
         {
             cout << "-1";
             return 0;
         }
-        for (int i = 0; i < n;i++)
+
+    for (int i = 0; i < n;i++)
+    {
+        if(!arr[i])
         {
-            
+            for (int j = 1; j < 4;j++)
+                if(arr[i-1]!=j && arr[i+1]!=j)
+                 {   arr[i] = j;
+                     break;}
         }
+    }
+
+    for (int i = 0; i < n;i++)
+        cout << arr[i] << " ";
 }
