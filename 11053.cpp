dec54e3f712295d6d6941e81b094ed6f5419fb3a
ksks723 <ksks723@gmail.com>
2022-04-09 22:14:39 +0900

    d

---
+++ 11053.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int arr[1001], dp[1001], a;
+int main()
+{
+    fast_io();
+    cin >> a;
+    for (int i = 0; i < a; i++)
+        cin >> arr[i];
+    for (int i = 0; i < a; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+            if (arr[i] > arr[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+    }
+
+    cout << *max_element(dp, dp + a);
+}
