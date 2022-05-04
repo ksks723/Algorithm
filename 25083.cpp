cf66cda866b74e96157e4ffc687964f17132f01b
ksks723 <ksks723@gmail.com>
2022-05-04 12:34:56 +0900

    d

--- 25083.cpp
+++ 25083.cpp
@@ -4,7 +4,7 @@ int main()
 {
 
     printf("         ,%c\'\"%d\n", 'r', 7);
-    printf("%c`-_   ,\'  ,/\n", 'r');
+    printf("%c`-_   ,\'  ,/\n", 'r');//
     printf(" \\. \". %c_%c\'\n", 'L', 'r');
     printf("   `~\\/\n");
     printf("      |\n");
