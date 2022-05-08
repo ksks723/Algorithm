1bd9e52dd8b93ff90bd2c735cf3a45fdb41b5c34
ksks723 <ksks723@gmail.com>
2022-05-08 23:53:04 +0900

    d

---
+++ 2747.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int a[45];
+
+int fibonacci(int n)
+{
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        a[i + 2] = a[i + 1] + a[i];
+    }
+    return a[n];
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << fibonacci(n) << "\n";
+}
