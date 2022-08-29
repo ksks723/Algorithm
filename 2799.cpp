75c136cdd221983a8932dc9cc82c4df839ac5697
ksks723 <ksks723@gmail.com>
2022-08-29 14:44:31 +0900

    d

---
+++ 2799.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int arr[1001], dp[1001], a;
+int main()
+{
+    fast_io();
+   
+}
